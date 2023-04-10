2db6572f3339790e1160aba4abb81ef06ef816b6
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-04-09 23:52:57 -0400

    fix for deployment

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext} from "react";
+import React, { useState, useContext} from "react";
 import axios from "axios";
 import { ShopContext } from "../../context/ShopContext";
 
