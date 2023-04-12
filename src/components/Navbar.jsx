ae2372374f2c845f2c35e8ecc64b28010e8b0401
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-04-12 00:10:27 -0400

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,10 +37,10 @@ export default function Navbar() {
 
 
     return(
-        <nav className="bg-blue-300 top-0 z-50 flex flex-wrap items-center justify-between px-2 py-3">
+        <nav className=" top-0 z-50 flex flex-wrap items-center justify-between px-2 py-3">
             <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                 <h1 className="px-2 text-5xl">
-                    <Link to={"/"}>trove</Link>
+                    <Link to={"/"}>shop</Link>
                 </h1>
                 <ul className="flex">
                     {jwt === null || jwt === "" ? <li className="px-2 text-lg"><Link to={"/login"}>Login</Link></li>: name}
