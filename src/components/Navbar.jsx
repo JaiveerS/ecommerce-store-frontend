3b6e36db526e018a84d6c83f6f3348d6f4795264
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-04-19 14:52:30 -0400

    added bacground click to change state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,7 +86,7 @@ export default function Navbar() {
                     </div>
                 </nav>
                 <div className={isOpen ? "navbar-menu relative z-50 block" : "hidden"}>
-                <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+                <div onClick={changeState} className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
                 <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
                     <div className="flex items-center mb-8">
                         <Hamburger color="black" toggled={isOpen} toggle={setIsOpen}/>
