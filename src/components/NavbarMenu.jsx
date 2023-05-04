4342d8aaa2d6948ae81e6c6ad41b1c8d3a938158
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2023-05-04 14:57:08 -0400

    added login and logout

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -2,13 +2,15 @@ import axios from "axios";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 import Hamburger from "hamburger-react"
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
 
 export default function NavbarMenu(props) {
     const {isOpen, setIsOpen} = props;
-    const {categoriesEndpoint} = useContext(ShopContext);
+    const {categoriesEndpoint, jwt, setJwt, setId} = useContext(ShopContext);
     const [categories, setCategories] = useState([]);
+    const navigate = useNavigate();
+
 
     function changeState(){
         setIsOpen(!isOpen);
@@ -19,6 +21,13 @@ export default function NavbarMenu(props) {
             console.log(response.data)
             setCategories(response.data)
         })
+    }
+
+    function signOut(){
+        setJwt("")
+        setId("")
+        navigate('/', {replace: true})
+        changeState()
     }
 
 
@@ -38,9 +47,6 @@ export default function NavbarMenu(props) {
                         <Link to={"/"} onClick={changeState} className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">Home</Link>
                     </li>
                     <li className="mb-1">
-                        <Link to={"/profile"} onClick={changeState} className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">Profile</Link>
-                    </li>
-                    <li className="mb-1">
                         <p className="block p-4 text-sm font-semibold text-gray-400 rounded">Categories</p>
                             <div>
                                 {categories.map((item => (
@@ -50,9 +56,25 @@ export default function NavbarMenu(props) {
                                 )))}                        
                             </div>
                     </li>
-                    <li className="mb-1">
+                    {/* <li className="mb-1">
                         <Link to={"/cart"} onClick={changeState} className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded">Cart</Link>
-                    </li>
+                    </li> */}
+                    {jwt === "" ?
+                    <div className="mt-auto">
+                        <div className="pt-6">
+                            <Link to={"/login"} onClick={changeState} className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl">Sign in</Link>
+                            <Link to={"/signup"} onClick={changeState} className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl">Sign Up</Link>
+                        </div>
+                    </div>
+                    : 
+                    <div className="">
+                        <li className="pt-6">
+                            <Link to={"/profile"} onClick={changeState} className="block p-4 mb-3 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded text-center">Profile</Link>
+                            <Link to={"/orders"} onClick={changeState} className="block p-4 mb-3 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded text-center">Orders</Link>
+                            <ul className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-red-600 hover:bg-red-700 rounded-xl cursor-pointer" onClick={signOut}>Log Out</ul>
+                        </li>
+                    </div>
+                    }
                 </ul>
             </div>
             {/* <div className="mt-auto">
