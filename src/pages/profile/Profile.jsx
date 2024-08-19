1969d5b6ae7ee6be0d83709659774ecf6b79b50f
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-08-19 16:19:23 -0400

    added user icon

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -19,6 +19,7 @@ export default function Profile(){
     return(
         <div className="flex flex-col items-center pt-10 min-h-screen">
             <h1 className="font-semibold text-xl pb-5">Profile</h1>
+            <img className="object-contain h-40 w-40" src="https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png" alt=""></img>
             <p>Name: {firstname} {lastname}</p>
             <p>Email: {email}</p>
         </div>
