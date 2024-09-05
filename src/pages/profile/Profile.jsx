7a322af4ab5c2196f67ebe67a4d0792e772c1f7f
jaiveer <jaiveer_@hotmail.com>
2024-09-04 21:08:40 -0400

    made profile image local

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react"
 import { ShopContext } from "../../context/ShopContext"
 import { useNavigate } from "react-router-dom";
+import profile from "../../assets/profile.jpg"
 
 
 
@@ -19,7 +20,7 @@ export default function Profile(){
     return(
         <div className="flex flex-col items-center pt-10 min-h-screen">
             <h1 className="font-semibold text-xl pb-5">Profile</h1>
-            <img className="object-contain h-40 w-40" src="https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png" alt=""></img>
+            <img className="object-contain h-40 w-40" src={profile} alt=""></img>
             <p>Name: {firstname} {lastname}</p>
             <p>Email: {email}</p>
         </div>
