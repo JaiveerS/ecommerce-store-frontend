d0f647aca5f98265c58677df14e6c80e8e5717e1
jaiveer <jaiveer_@hotmail.com>
2024-09-06 20:12:43 -0400

    added recommendations

---
+++ src/components/Recommendations.jsx
@@ -0,0 +1,61 @@
+import { useContext, useEffect, useState} from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext"
+import axios from "axios";
+import Product from "../pages/home/Product";
+
+
+export default function Recommendations({id}){
+    const [product, setProduct] = useState();
+    const {endpoint} = useContext(ShopContext);
+
+    //send id of item
+    function getRecommendations (){
+        axios.get(endpoint+ "/recommended", {params: {id: id}}).then((response)=> {
+            setProduct(response.data)
+        })
+    }
+
+    useEffect(() =>{
+        getRecommendations();
+    },[])
+
+
+    return (
+        <div className="">
+            {/* <div className="h-20 min-w-min max-w-screen-xl mx-auto px-2 py-6 "> */}
+            <div className="bg-white">
+                <div className="mx-auto max-w-2xl px-4 py-4 sm:px-4 sm:py-5 lg:max-w-7xl lg:px-10">
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        
+                        {product !== undefined ? product.map((product) => (
+                            <div key={product.id} className="group relative bg-gray-100 p-2 rounded">
+                                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-white lg:aspect-none group-hover:opacity-75 lg:h-80">
+                                    <img
+                                    alt="alt"
+                                    src={product.image}
+                                    className="h-64 w-full object-contain lg:h-full lg:w-full"
+                                    />
+                                </div>
+                                <div className="mt-4 flex justify-between">
+                                    <div>
+                                        <h3 className="text-sm text-gray-700">
+                                            <Link to={"/product/"+ product.id}>
+                                                <span aria-hidden="true" className="absolute inset-0" />
+                                                {product.productName}
+                                            </Link>
+                                        </h3>
+                                    </div>
+                                    <p className="text-sm font-medium text-gray-900">${product.price}.00</p>
+                                </div>
+                            </div>
+                        )): ""}
+                    </div> 
+                </div>
+                </div>
+            {/* </div> */}
+        </div>
+    )
+}
--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ShopContext } from "../../context/ShopContext";
 import image_not_found from "../../assets/image_not_found.png";
 import { useLocation } from 'react-router-dom';
+import Recommendations from "../../components/Recommendations";
 
 
 
@@ -33,19 +34,22 @@ export default function ProductPage(props) {
     return (
         <div className="min-h-screen">
         {product.price > 0 && (
-        <div className="flex justify-center flex-wrap">
-            <img className="object-contain h-96 w-96 mt-5 -mb-10" alt={product.description} src={product.image} onError={(e) => { e.target.src ={image_not_found}}}/>
-                <div className="px-10">
-                    <h1 className="pt-10 pb-5 font-semibold">{product.productName}</h1>
-                    <h3 className="py-1">Price: ${product.price}</h3>
-                    <h4 className="py-2">Category: {product.category}</h4>
-                    <p  className="py-2 max-w-md">{product.description}</p>
-                    <div className="flex py-4">
-                        <button onClick={() => addToCart(id)} className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-green-600 hover:text-white border-2 border-gray-900 focus:outline-none">
-                            Add To Cart{cartItemAmount > 0 && <>| {cartItemAmount} </>}
-                        </button>
+        <div className="flex flex-col justify-center">
+            <div className="flex justify-center flex-wrap">
+                <img className="object-contain h-96 w-96 mt-2" alt={product.description} src={product.image} onError={(e) => { e.target.src ={image_not_found}}}/>
+                    <div className="px-10">
+                        <h1 className="pt-10 pb-5 font-semibold">{product.productName}</h1>
+                        <h3 className="py-1">Price: ${product.price}.00</h3>
+                        <h4 className="py-2">Category: {product.category}</h4>
+                        <p  className="py-2 max-w-md">{product.description}</p>
+                        <div className="flex py-4">
+                            <button onClick={() => addToCart(id)} className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-green-600 hover:text-white border-2 border-gray-900 focus:outline-none">
+                                Add To Cart{cartItemAmount > 0 && <>| {cartItemAmount} </>}
+                            </button>
+                        </div>
                     </div>
-                </div>
+            </div>
+            <Recommendations id={id} />
         </div>
         )}
         </div>
