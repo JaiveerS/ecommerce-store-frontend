e78aacbc58c7645f8073f9f3a8b67e4a11b75e13
jaiveer <jaiveer_@hotmail.com>
2024-09-08 20:47:01 -0400

    adjusted styling

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -24,7 +24,7 @@ export default function Cart() {
         <div className="bg-white flex justify-center items-center">
         <div className="min-h-screen">
             <div className="flex flex-col">
-                <h1 className="text-3xl font-bold mb-2 pt-10 text-center">{cartQuantity  > 0 ? 'Shoping Cart' : 'Your Cart is empty.'}</h1>
+                <h1 className="text-3xl font-bold mb-2 pt-10 text-center">{cartQuantity  > 0 ? 'Shopping Cart' : 'Your Cart is empty.'}</h1>
                 {cartQuantity === 0 ? 
                     <button onClick={() => navigate("/", {replace:true})} className="bg-transparent hover:bg-blue-50 text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded self-center">
                         Continue Shopping
@@ -34,9 +34,18 @@ export default function Cart() {
                 {/* <Link className="hover:underline hover:font-bold text-center" to={"/"}>{cartQuantity  === 0 && ('start shopping')}</Link> */}
             </div>
             <div className="lg:space-y-0">
+
+                {/* {cartItems.map(item => {
+                    return <CartItem key ={product[item.id]} data ={product[item.id]} extra={CartItem.id}/>
+                })} */}
+{/* 
+                {cartItems.array.forEach(([key, value]) => {
+                    <p>key</p>
+                })} */}
+
                 {product.map((product) => {
-                        if(cartItems.get(product.id) !== 0){
-                            total += cartItems.get(product.id) * product.price;
+                        if(cartItems.has(product.id)){
+                            total += product.price * cartItems.get(product.id);
                             return <CartItem key={product.id} data={product} extra={cartItems.get(product.id)}/>
                         }
                         return ""
@@ -45,7 +54,7 @@ export default function Cart() {
                 }
                 {cartQuantity > 0 && (
                 <div>
-                    <h3 className="text-3xl font-bold mr-4 mb-6 pt-10 text-right" value="true">Total = ${total}</h3>
+                    <h3 className="text-3xl font-bold mr-4 mb-6 pt-10 text-right" value="true">Subtotal = ${total}.00</h3>
                     <button onClick={handleOnClick} className="float-right px-6 mr-3 py-2 mb-5 transition ease-in duration-200 uppercase rounded-full hover:bg-green-600 hover:text-white border-2 border-gray-900 focus:outline-none ">Checkout</button>
                 </div>)}
             </div>
